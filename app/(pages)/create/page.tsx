cdfd0eac785b406bd7732455a8cdc87b86599866
Kshitij Chauhan <kshitij.cx@gmail.com>
2025-01-01 20:42:53 +0530

    add create page ui

--- app/(pages)/create/page.tsx
+++ app/(pages)/create/page.tsx
@@ -1,4 +1,156 @@
-const page = () => {
-  return <div></div>;
+"use client";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useState } from "react";
+
+const formSchema = z.object({
+  name: z.string().min(2).max(50),
+  tables: z.coerce.number().min(1),
+  time: z.string({
+    required_error: "Please select an email to display.",
+  }),
+  email: z.string().email(),
+});
+
+const Create = () => {
+  const [date, setDate] = useState<Date>();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      tables: 1,
+      time: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    const formattedDate = date ? format(date, "dd-MM-yyyy") : "invalid";
+    const userData = { ...values, date: formattedDate };
+    console.log(userData);
+  }
+  return (
+    <div className="border rounded-xl shadow-xl p-6">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="Name" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="tables"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="No. of tables" {...field} type="number" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="Email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="time"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="Time Slot (open 10am - 4pm)" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="10:00 am">10:00 am</SelectItem>
+                      <SelectItem value="12:00 pm">12:00 pm</SelectItem>
+                      <SelectItem value="2:00 pm">2:00 pm</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="date"
+            render={() => (
+              <FormItem className="flex flex-col">
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-[280px] justify-start text-left font-normal",
+                        !date && "text-muted-foreground"
+                      )}
+                    >
+                      <CalendarIcon className="mr-2 h-4 w-4" />
+                      {date ? format(date, "PPP") : <span>Pick a date</span>}
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0">
+                    <Calendar
+                      mode="single"
+                      selected={date}
+                      onSelect={setDate}
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
 };
-export default page;
+export default Create;
